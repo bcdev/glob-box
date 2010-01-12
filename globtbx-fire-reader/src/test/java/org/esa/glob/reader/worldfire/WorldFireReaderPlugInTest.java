551385a3d4ba9327eefc527f0b18a6b9f2ff0226
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2010-01-12 14:42:03 +0000

    fixed test
    git-svn-id: https://www.brockmann-consult.de/svn/os/globtoolbox/trunk@7944 d90d2d48-2b33-0410-a973-e72f85796a02

--- globtbx-fire-reader/src/test/java/org/esa/glob/reader/worldfire/WorldFireReaderPlugInTest.java
+++ globtbx-fire-reader/src/test/java/org/esa/glob/reader/worldfire/WorldFireReaderPlugInTest.java
@@ -35,7 +35,7 @@ public class WorldFireReaderPlugInTest {
 
     @Test
     public void testDefaultFileExtensions() {
-        assertArrayContains("FIRE", plugIn.getDefaultFileExtensions());
+        assertArrayContains(".FIRE", plugIn.getDefaultFileExtensions());
     }
 
     @Test
