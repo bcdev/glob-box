f300d0699d77f44ce5fe92bfa86ed47754c9312c
Marco Peters <marco.peters@brockmann-consult.de>
2010-03-05 15:17:05 +0100

    - added core module - started with TimeSeries ToolView

---
+++ glob-ui/src/main/java/org/esa/beam/glob/ui/TimeSeriesDiagram.java
@@ -0,0 +1,5 @@
+package org.esa.beam.glob.ui;
+
+public class TimeSeriesDiagram {
+
+}
---
+++ glob-ui/src/main/java/org/esa/beam/glob/ui/TimeSeriesToolView.java
@@ -0,0 +1,14 @@
+package org.esa.beam.glob.ui;
+
+import org.esa.beam.framework.ui.application.support.AbstractToolView;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+public class TimeSeriesToolView extends AbstractToolView{
+
+    @Override
+    protected JComponent createControl() {
+        return new JPanel();
+    }
+}
