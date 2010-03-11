286a16fc35f0b37060b725da2e96eb73ee22069c
unknown <Thomas Storm@.(none)>
2010-03-11 11:42:42 +0100

    user now can specify range of displayed values by himself

--- glob-ui/src/main/java/org/esa/beam/glob/ui/TimeSeriesToolView.java
+++ glob-ui/src/main/java/org/esa/beam/glob/ui/TimeSeriesToolView.java
@@ -17,23 +17,34 @@ import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.Range;
 import org.jfree.data.time.Millisecond;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.time.TimeSeriesDataItem;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.event.InternalFrameAdapter;
 import javax.swing.event.InternalFrameEvent;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -53,7 +64,12 @@ public class TimeSeriesToolView extends AbstractToolView {
     private TimeSeries timeSeries;
     private List<RasterDataNode> availableBands = new ArrayList<RasterDataNode>();
     private ExecutorService executorService;
+    private static final String AUTO_MIN_MAX = "Auto min/max";
 
+    private final JTextField minInput = new JTextField(8);
+    private final JTextField maxInput = new JTextField(8);
+    private static final String MINIMUM_MUST_BE_DOUBLE = "<HTML><BODY>Minimum must be<BR>a double value</BODY></HTML>";
+    private static final String MAXIMUM_MUST_BE_DOUBLE = "<HTML><BODY>Maximum must be<BR>a double value</BODY></HTML>";
 
     public TimeSeriesToolView() {
         pixelPosListener = new TimeSeriesPPL();
@@ -85,6 +101,7 @@ public class TimeSeriesToolView extends AbstractToolView {
         domainAxis.setAutoRange(true);
         final ValueAxis rangeAxis = timeSeriesPlot.getRangeAxis();
         rangeAxis.setAutoRange(false);
+        rangeAxis.setUpperBound(1.0);
         XYItemRenderer r = timeSeriesPlot.getRenderer();
         if (r instanceof XYLineAndShapeRenderer) {
             XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
@@ -98,20 +115,89 @@ public class TimeSeriesToolView extends AbstractToolView {
         updateAvailableBands();
         updateUIState();
 
-        JPanel buttonPanel = new JPanel(new BorderLayout());
-        buttonPanel.setPreferredSize(new Dimension(100, 200));
+        final JPanel autoAdjustButtonPanel = new JPanel(new FlowLayout());
+        autoAdjustButtonPanel.setPreferredSize(new Dimension(120, 200));
 
-        JCheckBox autoAdjustBox = new JCheckBox( "Auto adjust" );
-        buttonPanel.add( BorderLayout.NORTH, autoAdjustBox );
-        autoAdjustBox.addActionListener( new ActionListener() {
+        final JCheckBox autoAdjustBox = new JCheckBox(AUTO_MIN_MAX);
+
+        final JPanel minPanel = new JPanel(new FlowLayout());
+        final JLabel minLabel = new JLabel("Min:");
+        final JPanel maxPanel = new JPanel(new FlowLayout());
+        final JLabel maxLabel = new JLabel("Max:");
+        final JPanel minErrorPanel = new JPanel(new FlowLayout());
+        final JLabel minErrorLabel = new JLabel("");
+        minErrorLabel.setForeground(Color.red);
+        minErrorPanel.add(minErrorLabel);
+
+        final JPanel maxErrorPanel = new JPanel(new FlowLayout());
+        final JLabel maxErrorLabel = new JLabel("");
+        maxErrorLabel.setForeground(Color.red);
+        maxErrorPanel.add(maxErrorLabel);
+
+        autoAdjustButtonPanel.add(autoAdjustBox);
+        autoAdjustBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 final boolean isSelected = ((JCheckBox) e.getSource()).isSelected();
+                minLabel.setEnabled(!isSelected);
+                maxLabel.setEnabled(!isSelected);
+                minInput.setEnabled(!isSelected);
+                maxInput.setEnabled(!isSelected);
+                if (isSelected) {
+                    minInput.setBorder(BorderFactory.createLineBorder(Color.gray));
+                    maxInput.setBorder(BorderFactory.createLineBorder(Color.gray));
+                } else {
+                    minInput.setBorder(BorderFactory.createLineBorder(Color.black));
+                    maxInput.setBorder(BorderFactory.createLineBorder(Color.black));
+                }
                 timeSeriesPlot.getRangeAxis().setAutoRange(isSelected);
             }
-        } );
+        });
 
-        control.add( BorderLayout.EAST, buttonPanel );
+        minPanel.setPreferredSize(new Dimension(120, 25));
+
+        minInput.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        minPanel.add(minLabel);
+        minPanel.add(minInput);
+
+        maxPanel.setPreferredSize(new Dimension(120, 25));
+
+        maxInput.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        minInput.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(final KeyEvent e) {
+                try {
+                    getTimeSeriesPlot().getRangeAxis().setLowerBound(Double.parseDouble(minInput.getText()));
+                    minErrorLabel.setText("");
+                } catch (final NumberFormatException ignore) {
+                    minErrorLabel.setText(MINIMUM_MUST_BE_DOUBLE);
+                }
+            }
+        });
+
+        maxInput.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(final KeyEvent e) {
+                try {
+                    getTimeSeriesPlot().getRangeAxis().setUpperBound(Double.parseDouble(maxInput.getText()));
+                    maxErrorLabel.setText("");
+                } catch (final NumberFormatException ignore) {
+                    maxErrorLabel.setText(MAXIMUM_MUST_BE_DOUBLE);
+                }
+            }
+        });
+
+        maxPanel.add(maxLabel);
+        maxPanel.add(maxInput);
+
+        autoAdjustButtonPanel.add(minPanel);
+        autoAdjustButtonPanel.add(maxPanel);
+        autoAdjustButtonPanel.add(minErrorPanel);
+        autoAdjustButtonPanel.add(maxErrorPanel);
+
+        control.add(BorderLayout.EAST, autoAdjustButtonPanel);
 
         return control;
     }
@@ -181,7 +267,7 @@ public class TimeSeriesToolView extends AbstractToolView {
         List<Product> productList = new ArrayList<Product>();
         if (sceneView != null) {
             final Product refProduct = sceneView.getProduct();
-            if(refProduct.getStartTime() == null) {
+            if (refProduct.getStartTime() == null) {
                 return productList;
             }
             final String productType = refProduct.getProductType();
@@ -318,11 +404,19 @@ public class TimeSeriesToolView extends AbstractToolView {
                                     MouseEvent e) {
             if (pixelPosValid && isActive()) {
                 getTimeSeriesPlot().setNoDataMessage(null);
-                if(timeSeries.isEmpty()) {
+                if (timeSeries.isEmpty()) {
                     initTimeSeries(getAvailableProducts());
                 }
                 executorService.submit(new TimeSeriesUpdater(pixelX, pixelY, currentLevel));
             }
+            final Range range = getTimeSeriesPlot().getRangeAxis().getRange();
+            double lowerBound = range.getLowerBound();
+            double upperBound = range.getUpperBound();
+
+            DecimalFormat df = new DecimalFormat("0.000000");
+
+            minInput.setText(df.format(lowerBound));
+            maxInput.setText(df.format(upperBound));
         }
 
         @Override
