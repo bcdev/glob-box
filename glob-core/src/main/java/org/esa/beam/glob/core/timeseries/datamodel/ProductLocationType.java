0bea933244583abfd902e40af959e19a1179ab26
thomasstorm <thomas_storm@brockmann-consult.de>
2010-06-24 13:47:25 +0200

    allow to search directories (recursively)

--- glob-core/src/main/java/org/esa/beam/glob/core/timeseries/datamodel/ProductLocationType.java
+++ glob-core/src/main/java/org/esa/beam/glob/core/timeseries/datamodel/ProductLocationType.java
@@ -3,7 +3,9 @@ package org.esa.beam.glob.core.timeseries.datamodel;
 import org.esa.beam.framework.dataio.ProductIO;
 import org.esa.beam.framework.datamodel.Product;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -15,23 +17,57 @@ import java.util.List;
  */
 public enum ProductLocationType {
 
-    FILE{
+    FILE {
         @Override
         public List<Product> findProducts(String path) {
-            try {
-                final Product product = ProductIO.readProduct(path);
-                if (product != null) {
-                    return Arrays.asList(product);
-                }
-            } catch (IOException ignore) {
-            }
-            return Collections.emptyList();
+            return readSingleProduct(new File(path));
         }
     },
-    DIRECTORY,
-    DIRECTORY_REC;
+    DIRECTORY {
+        @Override
+        public List<Product> findProducts(String path) {
+            List<Product> products = new ArrayList<Product>();
+            for (File file : listFiles(path)) {
+                if (!file.isDirectory()) {
+                    products.addAll(readSingleProduct(file));
+                }
+            }
+            return products;
+        }
+    },
+    DIRECTORY_REC {
+        @Override
+        public List<Product> findProducts(String path) {
+            List<Product> products = new ArrayList<Product>();
+            for (File file : listFiles(path)) {
+                if (file.isDirectory()) {
+                    products.addAll(findProducts(file.getPath()));
+                } else {
+                    products.addAll(readSingleProduct(file));
+                }
+            }
+            return products;
+        }
+    };
 
-    public List<Product> findProducts(String path) {
+    abstract public List<Product> findProducts(String path);
+
+    private static File[] listFiles(String path) {
+        File dir = new File(path);
+        if (!dir.isDirectory()) {
+            throw new IllegalArgumentException("The given path is not a directory: " + path);
+        }
+        return dir.listFiles();
+    }
+
+    private static List<Product> readSingleProduct(File path) {
+        try {
+            final Product product = ProductIO.readProduct(path);
+            if (product != null) {
+                return Arrays.asList(product);
+            }
+        } catch (IOException ignore) {
+        }
         return Collections.emptyList();
     }
 }
