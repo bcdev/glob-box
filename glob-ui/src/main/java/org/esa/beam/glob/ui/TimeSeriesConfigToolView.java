60c5da95d960092fe4df1d6e50e89fa928bce499
Marco Peters <marco.peters@brockmann-consult.de>
2010-06-25 09:28:11 +0200

    TimeSeriesConfigToolView now listening on ProductTree

--- glob-ui/src/main/java/org/esa/beam/glob/ui/TimeSeriesConfigToolView.java
+++ glob-ui/src/main/java/org/esa/beam/glob/ui/TimeSeriesConfigToolView.java
@@ -2,47 +2,82 @@ package org.esa.beam.glob.ui;
 
 import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.datamodel.ProductNode;
-import org.esa.beam.framework.datamodel.RasterDataNode;
-import org.esa.beam.framework.ui.product.ProductSceneView;
+import org.esa.beam.framework.ui.AppContext;
+import org.esa.beam.framework.ui.application.support.AbstractToolView;
 import org.esa.beam.framework.ui.product.ProductTreeListenerAdapter;
 import org.esa.beam.visat.VisatApp;
-import org.esa.beam.visat.toolviews.layermanager.AbstractLayerToolView;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 import java.util.WeakHashMap;
 
 
 // todo (mp 20100624) If variable is removed view must be closed
 
-public class TimeSeriesConfigToolView extends AbstractLayerToolView {
-    public static final String ID = "timeSeriesConfigToolView";
+public class TimeSeriesConfigToolView extends AbstractToolView {
 
-    private WeakHashMap<ProductSceneView, TimeSeriesConfigForm> formMap;
+    private AppContext appContext;
+    private JPanel controlPanel;
+    private Product selectedProduct;
+    private String prefixTitle;
+
+    private WeakHashMap<Product, TimeSeriesConfigForm> formMap;
     private TimeSeriesConfigForm activeForm;
 
     public TimeSeriesConfigToolView() {
-        formMap = new WeakHashMap<ProductSceneView, TimeSeriesConfigForm>();
+        formMap = new WeakHashMap<Product, TimeSeriesConfigForm>();
+        appContext = VisatApp.getApp();
+    }
+
+    protected JPanel getControlPanel() {
+        return controlPanel;
     }
 
     @Override
     protected JComponent createControl() {
-        // todo (mp 20100624) Product Tree listener not yet implemented
-        VisatApp.getApp().addProductTreeListener(new TimeSeriesPTL());
-        final JComponent control = super.createControl();
+        controlPanel = new JPanel(new BorderLayout());
+        controlPanel.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+        prefixTitle = getDescriptor().getTitle();
+
+        setSelectedProduct(appContext.getSelectedProduct());
+
+        VisatApp.getApp().addProductTreeListener(new TSConfigPTL());
+
         realizeActiveForm();
-        return control;
+        updateTitle();
+        return controlPanel;
     }
 
-    @Override
-    protected void viewClosed(ProductSceneView view) {
-        formMap.remove(view);
+    private void updateTitle() {
+        final String suffix;
+        final Product product = getSelectedProduct();
+        if (product != null) {
+            suffix = " - " + product.getDisplayName();
+        } else {
+            suffix = "";
+        }
+        getDescriptor().setTitle(prefixTitle + suffix);
     }
 
-    @Override
-    protected void viewSelectionChanged(ProductSceneView oldView, ProductSceneView newView) {
-        realizeActiveForm();
+    public Product getSelectedProduct() {
+        return selectedProduct;
+    }
+
+    private void setSelectedProduct(Product newProduct) {
+        Product oldProduct = selectedProduct;
+        if (newProduct != oldProduct) {
+            selectedProduct = newProduct;
+            realizeActiveForm();
+            updateTitle();
+        }
+    }
+
+    protected void productClosed(Product product) {
+        formMap.remove(product);
+        setSelectedProduct(null);
     }
 
     private void realizeActiveForm() {
@@ -52,34 +87,34 @@ public class TimeSeriesConfigToolView extends AbstractLayerToolView {
             controlPanel.remove(0);
         }
 
-        activeForm = getOrCreateActiveForm(getSelectedView());
+        activeForm = getOrCreateActiveForm(getSelectedProduct());
         controlPanel.add(activeForm.getControl(), BorderLayout.CENTER);
 
         controlPanel.validate();
         controlPanel.repaint();
     }
 
-    protected TimeSeriesConfigForm getOrCreateActiveForm(ProductSceneView view) {
-        if (formMap.containsKey(view)) {
-            activeForm = formMap.get(view);
+    protected TimeSeriesConfigForm getOrCreateActiveForm(Product product) {
+        if (formMap.containsKey(product)) {
+            activeForm = formMap.get(product);
         } else {
             activeForm = new TimeSeriesConfigForm();
-            formMap.put(view, activeForm);
-        }
-        Product product = null;
-        if(view != null) {
-            product = view.getProduct();
+            formMap.put(product, activeForm);
         }
         activeForm.updateFormControl(product);
         return activeForm;
     }
 
-     private class TimeSeriesPTL extends ProductTreeListenerAdapter {
+    private class TSConfigPTL extends ProductTreeListenerAdapter {
+
+        @Override
+        public void productRemoved(Product product) {
+            productClosed(product);
+        }
 
         @Override
         public void productNodeSelected(ProductNode productNode, int clickCount) {
-            if (productNode instanceof RasterDataNode) {
-            }
+            setSelectedProduct(productNode.getProduct());
         }
     }
 }
