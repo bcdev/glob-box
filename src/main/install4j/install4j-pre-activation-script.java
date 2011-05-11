2f255db0101a3b331e5f57aada24ae8995b05efa
marpet <marco.peters@brockmann-consult.de>
2011-05-11 16:32:03 +0200

    updated installers for new install4j version

--- src/main/install4j/install4j-pre-activation-script.java
+++ src/main/install4j/install4j-pre-activation-script.java
@@ -1,17 +1,29 @@
 /**
  * This script is also included in globtoolbox.install4j. It is here only
- * for review and reformatting.
+ * for review and reformatting. (Installer -> Installation -> Screen activation -> Pre-activation script)
  */
 File modulesDir = new File(context.getInstallationDirectory(), "modules");
 File[] gtModuleFiles = modulesDir.listFiles(new FileFilter() {
     public boolean accept(File file) {
         String name = file.getName();
-        return name.startsWith("globtbx-"));
+        return name.startsWith("beam-arcbingrid-reader") ||
+				name.startsWith("beam-envi-reader") ||
+				name.startsWith("beam-globaerosol-reader") ||
+				name.startsWith("beam-globcarbon-reader") ||
+				name.startsWith("beam-globcolour-reader") ||
+				name.startsWith("beam-globcover-reader") ||
+				name.startsWith("beam-igbp-glcc-reader") ||
+				name.startsWith("beam-medspiration-reader") ||
+				name.startsWith("beam-worldfire-reader") ||
+				name.startsWith("glob-core") ||
+				name.startsWith("glob-export") ||
+				name.startsWith("glob-help") ||
+				name.startsWith("glob-ui");
     }
 });
 
 if (gtModuleFiles != null && gtModuleFiles.length > 0) {
-    StringBuilder sb = new StringBuilder("Following existing BEAM GlobToolbox Reader Pack modules will be removed:\n");
+    StringBuilder sb = new StringBuilder("Following existing BEAM GlobToolbox modules will be removed:\n");
     for (int i = 0; i < gtModuleFiles.length; i++) {
         File file = gtModuleFiles[i];
         sb.append(" " + file.getName() + "\n");
@@ -48,7 +60,7 @@ if (gtModuleFiles != null && gtModuleFiles.length > 0) {
     }
 
     if (sb.length() > 0) {
-        Util.showMessage("Following existing BEAM GlobToolbox Reader Pack modules could not be removed:\n"
+        Util.showMessage("Following existing BEAM GlobToolbox modules could not be removed:\n"
                 + sb + "Please remove them manually from the BEAM modules directory at\n"
                 + modulesDir);
     }
