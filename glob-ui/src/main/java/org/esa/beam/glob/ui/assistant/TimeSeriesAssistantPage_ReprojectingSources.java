36cd6a794d2b8a377a2e306323684a8c15831ace
thomasstorm <thomas_storm@brockmann-consult.de>
2011-11-10 16:36:58 +0100

    Fixed Issue #4

--- glob-ui/src/main/java/org/esa/beam/glob/ui/assistant/TimeSeriesAssistantPage_ReprojectingSources.java
+++ glob-ui/src/main/java/org/esa/beam/glob/ui/assistant/TimeSeriesAssistantPage_ReprojectingSources.java
@@ -1,5 +1,7 @@
 package org.esa.beam.glob.ui.assistant;
 
+import com.bc.ceres.core.ProgressMonitor;
+import com.bc.ceres.swing.progress.ProgressMonitorSwingWorker;
 import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.gpf.GPF;
 import org.esa.beam.framework.ui.assistant.AssistantPage;
@@ -41,62 +43,79 @@ class TimeSeriesAssistantPage_ReprojectingSources extends AbstractTimeSeriesAssi
 
     @Override
     public AssistantPage getNextPage() {
-        reprojectSourceProducts();
+        final Reprojector reprojector = new Reprojector(this.getPageComponent());
+        reprojector.executeWithBlocking();
         return new TimeSeriesAssistantPage_VariableSelection(getAssistantModel());
     }
-
-    private void reprojectSourceProducts() {
-        final ProductLocationsPaneModel productLocationsModel = getAssistantModel().getProductLocationsModel();
-        final List<ProductLocation> productLocations = productLocationsModel.getProductLocations();
-        for (ProductLocation productLocation : productLocations) {
-            final Map<String, Product> products = productLocation.getProducts();
-            final Product crsReferenceProduct = getCrsReferenceProduct();
-            for (Map.Entry<String, Product> productEntry : products.entrySet()) {
-                final Product product = productEntry.getValue();
-                if (!product.isCompatibleProduct(crsReferenceProduct, 0.1E-4f)) {
-                    Product reprojectedProduct = createProjectedProduct(product, crsReferenceProduct);
-                    productEntry.setValue(reprojectedProduct);
-                }
-            }
-        }
-    }
-
-    private Product createProjectedProduct(Product toReproject, Product crsReference) {
-        final Map<String, Product> productMap = getProductMap(toReproject, crsReference);
-        final Map<String, Object> parameterMap = new HashMap<String, Object>();
-        parameterMap.put("resamplingName", "Nearest");
-        parameterMap.put("includeTiePointGrids", false);
-        parameterMap.put("addDeltaBands", false);
-        // @todo - generalise
-        final Product reprojectedProduct = GPF.createProduct("Reproject", parameterMap, productMap);
-        reprojectedProduct.setStartTime(toReproject.getStartTime());
-        reprojectedProduct.setEndTime(toReproject.getEndTime());
-        return reprojectedProduct;
-    }
-
-
-    private Map<String, Product> getProductMap(Product product, Product crsReference) {
-        final Map<String, Product> productMap = new HashMap<String, Product>(2);
-        productMap.put("source", product);
-        productMap.put("collocateWith", crsReference);
-        return productMap;
-    }
-
 
     @Override
     protected Component createPageComponent() {
         return new JLabel("Da kommt noch was");
     }
 
-    private Product getCrsReferenceProduct() {
-        final List<ProductLocation> productLocations = getAssistantModel().getProductLocationsModel().getProductLocations();
-        for (ProductLocation productLocation : productLocations) {
-            for (Product product : productLocation.getProducts().values()) {
-                if(product != null) {
-                    return product;
+    private class Reprojector extends ProgressMonitorSwingWorker<Void, TimeSeriesAssistantModel> {
+
+        protected Reprojector(Component parentComponent) {
+            super(parentComponent, "Reprojecting source products ...");
+        }
+
+        @Override
+        protected Void doInBackground(ProgressMonitor pm) throws Exception {
+            reprojectSourceProducts(pm);
+            return null;
+        }
+
+        private void reprojectSourceProducts(ProgressMonitor pm) {
+            final ProductLocationsPaneModel productLocationsModel = getAssistantModel().getProductLocationsModel();
+            final List<ProductLocation> productLocations = productLocationsModel.getProductLocations();
+            pm.beginTask("Reprojecting...", productLocations.size());
+            for (ProductLocation productLocation : productLocations) {
+                final Map<String, Product> products = productLocation.getProducts();
+                final Product crsReferenceProduct = getCrsReferenceProduct();
+                for (Map.Entry<String, Product> productEntry : products.entrySet()) {
+                    final Product product = productEntry.getValue();
+                    if (!product.isCompatibleProduct(crsReferenceProduct, 0.1E-4f)) {
+                        Product reprojectedProduct = createProjectedProduct(product, crsReferenceProduct);
+                        productEntry.setValue(reprojectedProduct);
+                    }
+                }
+                pm.worked(1);
+            }
+            pm.done();
+        }
+
+        private Product createProjectedProduct(Product toReproject, Product crsReference) {
+            final Map<String, Product> productMap = getProductMap(toReproject, crsReference);
+            final Map<String, Object> parameterMap = new HashMap<String, Object>();
+            parameterMap.put("resamplingName", "Nearest");
+            parameterMap.put("includeTiePointGrids", false);
+            parameterMap.put("addDeltaBands", false);
+            // @todo - generalise
+            final Product reprojectedProduct = GPF.createProduct("Reproject", parameterMap, productMap);
+            reprojectedProduct.setStartTime(toReproject.getStartTime());
+            reprojectedProduct.setEndTime(toReproject.getEndTime());
+            return reprojectedProduct;
+        }
+
+
+        private Map<String, Product> getProductMap(Product product, Product crsReference) {
+            final Map<String, Product> productMap = new HashMap<String, Product>(2);
+            productMap.put("source", product);
+            productMap.put("collocateWith", crsReference);
+            return productMap;
+        }
+
+
+        private Product getCrsReferenceProduct() {
+            final List<ProductLocation> productLocations = getAssistantModel().getProductLocationsModel().getProductLocations();
+            for (ProductLocation productLocation : productLocations) {
+                for (Product product : productLocation.getProducts().values()) {
+                    if (product != null) {
+                        return product;
+                    }
                 }
             }
+            return null;
         }
-        return null;
     }
 }
