fc3dbc344911fd311c768d21d13ab83b91f5aa8e
thomasstorm <thomas_storm@brockmann-consult.de>
2011-11-15 14:58:29 +0100

    closing old insitu sources when loading new one

--- glob-ui/src/main/java/org/esa/beam/glob/ui/manager/LoadInsituAction.java
+++ glob-ui/src/main/java/org/esa/beam/glob/ui/manager/LoadInsituAction.java
@@ -16,14 +16,11 @@
 
 package org.esa.beam.glob.ui.manager;
 
-import org.esa.beam.framework.datamodel.ProductNodeEvent;
 import org.esa.beam.framework.ui.UIUtils;
 import org.esa.beam.glob.core.insitu.InsituLoader;
 import org.esa.beam.glob.core.insitu.InsituLoaderFactory;
 import org.esa.beam.glob.core.insitu.InsituSource;
 import org.esa.beam.glob.core.timeseries.datamodel.AbstractTimeSeries;
-import org.esa.beam.glob.core.timeseries.datamodel.TimeSeriesChangeEvent;
-import org.esa.beam.glob.core.timeseries.datamodel.TimeSeriesListener;
 import org.esa.beam.util.PropertyMap;
 import org.esa.beam.util.SystemUtils;
 import org.esa.beam.util.io.BeamFileChooser;
@@ -49,6 +46,7 @@ public class LoadInsituAction extends AbstractAction {
     private static final String PROPERTY_KEY_LAST_OPEN_INSITU_DIR = "glob.file.lastInsituOpenDir";
 
     private final AbstractTimeSeries currentTimeSeries;
+    private InsituSource insituSource;
 
     public LoadInsituAction(AbstractTimeSeries currentTimeSeries) {
         putValue(SHORT_DESCRIPTION, "Import in situ source file");
@@ -77,34 +75,11 @@ public class LoadInsituAction extends AbstractAction {
         final File selectedFile = fileChooser.getSelectedFile();
         try {
             final InsituLoader insituLoader = InsituLoaderFactory.createInsituLoader(selectedFile);
-            final InsituSource insituSource = new InsituSource(insituLoader);
+            if(insituSource != null) {
+                insituSource.close();
+            }
+            insituSource = new InsituSource(insituLoader);
             currentTimeSeries.setInsituSource(insituSource);
-            currentTimeSeries.addTimeSeriesListener(new TimeSeriesListener() {
-                @Override
-                public void nodeAdded(ProductNodeEvent event) {
-                    System.out.println("nodeAdded = " + event.getType());
-                }
-
-                @Override
-                public void nodeChanged(ProductNodeEvent event) {
-                    System.out.println("nodeChanged = " + event.getType());
-                }
-
-                @Override
-                public void nodeDataChanged(ProductNodeEvent event) {
-                    System.out.println("nodeDataChanged = " + event.getType());
-                }
-
-                @Override
-                public void nodeRemoved(ProductNodeEvent event) {
-                    System.out.println("nodeRemoved = " + event.getType());
-                }
-
-                @Override
-                public void timeSeriesChanged(TimeSeriesChangeEvent event) {
-                    System.out.println("event = " + event.getType());
-                }
-            });
         } catch (IOException exception) {
             BeamLogManager.getSystemLogger().log(Level.WARNING, "Unable to load in situ data from '" + selectedFile + "'.", exception);
             return;
